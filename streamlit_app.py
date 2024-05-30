2799156b0a87cc9c6f70b8ff1e198d585b691d4c
Sindre Wetting <101550155+sindrewetting@users.noreply.github.com>
2024-05-30 12:43:06 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,7 +13,9 @@ st.write(
 name_on_order = st.text_input ('Name on Smoothie:')
 st.write('The name on your Smoothie will be:', name_on_order)
 
-session = get_active_session()
+
+cnx = st.connection(snowflake)
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 ingredients_list = st.multiselect('Choose up to 5 ingredients:', my_dataframe, max_selections=5)
